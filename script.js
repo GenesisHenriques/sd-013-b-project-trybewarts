afc9a4fc2a70222195a00a6d3c5de954640038af
Tenari <guilherme.tenarie@gmail.com>
2021-06-16 17:55:15 -0300

    Adiciona requisito 3

---
+++ script.js
@@ -0,0 +1,23 @@
+const loginButton = document.getElementById('login-button');
+
+loginButton.addEventListener('click', checkUser);
+
+function checkUser() {
+  const userLogin = document.getElementById('user-login');
+  const userPassword = document.getElementById('user-password');
+  const user = userLogin.value;
+  const password = userPassword.value;
+  let email = 'tryber@teste.com';
+  const senha = '123456';
+  if (user === email && password === senha) {
+    alert('Olá, Tryber!');
+  } else {
+    alert('Login ou senha inválidos.');
+  }
+}
+
+//  if (userLogin !== 123) {
+//     console.log('invalido')
+//  }
+// console.log(userLogin)
+// }
