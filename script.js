02a4445948a61d9b5fadc3a3083201b49a73cb62
Vinicius <vinicius.10.carvalho.100@gmail.com>
2021-06-16 20:23:21 -0300

    arrumando comentario no js

--- script.js
+++ script.js
@@ -1,11 +1,11 @@
-/* document.querySelector('.login').addEventListener('click', () => {
+document.querySelector('.login').addEventListener('click', () => {
   const inputLogin = document.getElementById('login').value;
   const inputSenha = document.getElementById('senha').value;
 
   return inputLogin !== 'tryber@teste.com' || inputSenha !== '123456'
     ? alert('Login ou senha inválidos.')
     : alert('Olá, Tryber!');
-}); */
+});
 
 document.getElementById('agreement').addEventListener('click', () => {
   const check = document.getElementById('agreement');
