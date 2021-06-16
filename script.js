241d9fd570264ba61587ee4b670c1b5cfeb11550
Alef Sloan <Alef_sloan@hotmail.com>
2021-06-16 17:16:35 -0300

    Requisito 6 finalizado. Lint respeitado.

--- script.js
+++ script.js
@@ -1,15 +1,15 @@
 const loginBtn = document.getElementById('btn-login');
 
 function canLogin(event) {
-    const inputLogin = document.getElementById('input-login')
-    const inputPasswrd = document.getElementById('input-password')
-    if (inputLogin.value === 'tryber@teste.com' && inputPasswrd.value === '123456') {
-        alert('Olá, Tryber!')
-        event.preventDefault();
-    } else {
-    alert('Login ou senha inválidos.')
+  const inputLogin = document.getElementById('input-login');
+  const inputPasswrd = document.getElementById('input-password');
+  if (inputLogin.value === 'tryber@teste.com' && inputPasswrd.value === '123456') {
+    alert('Olá, Tryber!');
     event.preventDefault();
-    }
+  } else {
+    alert('Login ou senha inválidos.');
+    event.preventDefault();
+  }
 }
 
 loginBtn.addEventListener('click', canLogin);
