ad18f78f61a8dd4f8960732d72c5b30bf1d16582
Matteus Fernandes <matteus_gbi@hotmail.com>
2021-06-16 20:01:22 -0300

    Resolve os problemas do lint

--- script.js
+++ script.js
@@ -12,14 +12,14 @@ loginButton.addEventListener('click', () => {
   }
 });
 
-const  botao = document.getElementById("submit-btn");
+const botao = document.getElementById('submit-btn');
 botao.disabled = true;
-const checkbox = document.getElementById("agreement");
-checkbox.addEventListener("input", function(event){
-    var conteudo = document.getElementById("agreement");
-    if (conteudo.checked === true) {
-      botao.disabled = false;
-    } else {
-      botao.disabled = true;
-    }
+const checkbox = document.getElementById('agreement');
+checkbox.addEventListener('input', () => {
+  const conteudo = document.getElementById('agreement');
+  if (conteudo.checked === true) {
+    botao.disabled = false;
+  } else {
+    botao.disabled = true;
+  }
 });
