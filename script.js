cc4ebc0987ebae989c80a033a875d4bc7bc62e72
GenesisHenriques <genesishenriques7@gmail.com>
2021-06-16 16:58:17 -0300

    Requisitos 1 e 2

--- script.js
+++ script.js
@@ -1,7 +1,31 @@
-//<!-- Ínicio de Trecho de Código do Genesis -->
+// Ínicio de Trecho de Código do Genesis
+document.getElementById("btnLogin").addEventListener("click", function(event){
+  event.preventDefault()
+  validatingLogin();
+});
 
-//<!-- Fim de Trecho de Código do Genesis -->
 
-//<!-- Inicio de Trecho de código do Alan -->
 
-//<!-- Fim de Trecho de Código do Alan -->
+function validatingLogin() {
+  
+  alert('foi')
+
+  const login = document.getElementById('login');
+  const password = document.getElementById('password');
+
+  if (login.value === 'tryber@teste.com' && password.value === '123456') {
+  alert('Olá, Tryber!');
+  } else {
+  alert('Login ou senha inválidos.');
+  }
+}
+
+
+
+
+
+// Fim de Trecho de Código do Genesis
+
+// Inicio de Trecho de código do Alan
+
+// Fim de Trecho de Código do Alan
