8451940eb12cc0b56b4c7bce98c4354ba34d6ea4
GenesisHenriques <genesishenriques7@gmail.com>
2021-06-16 15:18:01 -0300

    Separaçao de trecho de codigo no CSS e Js

---
+++ script.js
@@ -0,0 +1,7 @@
+//<!-- Ínicio de Trecho de Código do Genesis -->
+
+//<!-- Fim de Trecho de Código do Genesis -->
+
+//<!-- Inicio de Trecho de código do Alan -->
+
+//<!-- Fim de Trecho de Código do Alan -->
