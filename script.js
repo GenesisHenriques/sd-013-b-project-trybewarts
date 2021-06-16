e9a618e13e49b8f09322cfc92ea9c00eb17505d7
Gustavo <axt872@gmail.com>
2021-06-16 16:19:14 -0300

    Corrigindo linter

--- script.js
+++ script.js
@@ -1,18 +1,15 @@
-const loginButton = document.getElementById('login-button')
-const loginUsername = document.querySelector('#login-username')
-const loginPassword = document.querySelector('#login-password')
+const loginButton = document.getElementById('login-button');
+const loginUsername = document.querySelector('#login-username');
+const loginPassword = document.querySelector('#login-password');
 
 function loginVerification() {
-  if (loginUsername.value != 'tryber@teste.com') {
-    alert('Login ou senha inválidos.')
-  }
-  else if (loginPassword.value != '123456') {
-    alert('Login ou senha inválidos.')
-  }
-  else {
-    alert('Olá, Tryber!')
+  if (loginUsername.value !== 'tryber@teste.com') {
+    alert('Login ou senha inválidos.');
+  } else if (loginPassword.value !== '123456') {
+    alert('Login ou senha inválidos.');
+  } else {
+    alert('Olá, Tryber!');
   }
 }
 
-loginButton.addEventListener('click', loginVerification)
-
+loginButton.addEventListener('click', loginVerification);
