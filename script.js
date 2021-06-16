b6f5a5c99943cef2aa059c14fe843940fe2d77e0
Tenari <guilherme.tenarie@gmail.com>
2021-06-16 19:05:42 -0300

    Refatoraçao de script.js para ESlint

--- script.js
+++ script.js
@@ -1,13 +1,11 @@
 const loginButton = document.getElementById('login-button');
 
-loginButton.addEventListener('click', checkUser);
-
 function checkUser() {
   const userLogin = document.getElementById('user-login');
   const userPassword = document.getElementById('user-password');
   const user = userLogin.value;
   const password = userPassword.value;
-  let email = 'tryber@teste.com';
+  const email = 'tryber@teste.com';
   const senha = '123456';
   if (user === email && password === senha) {
     alert('Olá, Tryber!');
@@ -15,3 +13,5 @@ function checkUser() {
     alert('Login ou senha inválidos.');
   }
 }
+
+loginButton.addEventListener('click', checkUser);
