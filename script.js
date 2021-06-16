e87cb7356ab77494e9ddc4b4623f9743cee415f8
Arthur-Jr <arthurjr1906@gmail.com>
2021-06-16 18:04:33 -0300

    Adiciona o vigésimo requisto.

--- script.js
+++ script.js
@@ -2,6 +2,9 @@ const inputLogin = document.querySelector('#login');
 const inputPassword = document.querySelector('#password');
 const agreement = document.querySelector('#agreement');
 const button = document.querySelector('#submit-btn');
+const textArea = document.querySelector('#textarea');
+const counter = document.querySelector('#counter');
+
 
 document.querySelector('#btn-login').addEventListener('click', () => {
   if (inputLogin.value === 'tryber@teste.com' && inputPassword.value === '123456') {
@@ -20,3 +23,23 @@ agreement.addEventListener('change', () => {
   // Aprendi sobre button.disabled, neste link
   // https://flexiple.com/disable-button-javascript/
 });
+
+
+textArea.addEventListener('keypress', () => {
+  if (counter.innerText > 0) {
+    counter.innerText = parseInt(counter.innerText, 10) - 1;
+  }
+});
+
+textArea.addEventListener('keyup', () => {
+  if (textArea.value === '' && counter.innerText < 500) {
+    counter.innerText = '500';
+  } 
+});
+
+textArea.addEventListener('keydown', (e) => {
+  if (e.key === 'Backspace' && counter.innerText < 500) {
+    counter.innerText = parseInt(counter.innerText, 10) + 1;
+  }
+});
+  
