5492a26ff7e61a9e01c49b75d93058c671e382e2
Cassio Mascarenhas <csmlvx@gmail.com>
2021-06-16 20:32:01 -0300

    Adicionado requisito 18

--- script.js
+++ script.js
@@ -12,3 +12,20 @@ function loginVerify() {
 
 const logar = document.getElementById('logar');
 logar.addEventListener('click', loginVerify);
+
+function checkVerify() {
+  
+  const checkbox = document.getElementById('agreement');
+  const btn = document.getElementById('submit-btn');
+
+  if(checkbox.checked) {      
+    btn.removeAttribute("disabled","disabled");
+  } else {
+    btn.setAttribute("disabled", "disabled");
+  }
+}
+
+checkVerify();
+
+const agree = document.getElementById('agreement');
+agree.addEventListener('click', checkVerify);
