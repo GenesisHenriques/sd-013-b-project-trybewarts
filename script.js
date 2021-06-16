4b8757c3ebda3a77c2871a933641920c3247ba9a
Arthur-Jr <arthurjr1906@gmail.com>
2021-06-16 15:58:38 -0300

    Adiciona html do nono requisito.

--- script.js
+++ script.js
@@ -3,8 +3,8 @@ const inputPassword = document.querySelector('#password');
 
 document.querySelector('#btn-login').addEventListener('click', () => {
   if (inputLogin.value === 'tryber@teste.com' && inputPassword.value === '123456') {
-    alert ('Olá, Tryber!');
+    alert('Olá, Tryber!');
   } else {
-    alert ('Login ou senha inválidos.');
+    alert('Login ou senha inválidos.');
   }
 });
