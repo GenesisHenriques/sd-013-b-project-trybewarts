0fba3e4708d7c0df248b4b2b0065efef55f0606f
Vinicius <viniderp@gmail.com>
2021-06-16 18:05:36 -0300

    Requisitos 14-19

--- script.js
+++ script.js
@@ -1,6 +1,10 @@
 const btnLogin = document.querySelector('.trybewarts-login button');
 const inputLoginEmail = document.querySelector('.input-login-email');
 const inputLoginPassword = document.querySelector('.input-login-password');
+const checkbox = document.querySelector('#agreement');
+const btnSubmit = document.querySelector('#submit-btn');
+
+btnSubmit.disabled = true;
 
 const authLogin = () => {
   if (inputLoginEmail.value !== 'tryber@teste.com') return false;
@@ -19,5 +23,15 @@ btnLogin.addEventListener('click', (e) => {
     alert('Olá, Tryber!');
   } else {
     alert('Login ou senha inválidos.');
+  }
+});
+
+checkbox.addEventListener('change', (e) => {
+  e.preventDefault();
+
+  if (checkbox.checked) {
+    btnSubmit.disabled = false;
+  } else {
+    btnSubmit.disabled = true;
   }
 });
