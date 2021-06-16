507944d46c7378b200cd63d1ab34c8e235d7c6d1
Paulo Lima <paulynho_lima@hotmail.com>
2021-06-16 18:53:56 -0300

    atualizando

---
+++ script.js
@@ -0,0 +1,7 @@
+let button = document.querySelector('.button-login');
+button.addEventListener('click', logar);
+
+let input = document.getElementsByClassName('trybewarts-login')
+function logar() {
+ 
+}
