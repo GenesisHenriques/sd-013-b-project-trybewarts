310a4c50ca01aba4459b2700bc952a96722b9abf
Rodrigo <rodrigo.fidencio@gmail.com>
2021-06-16 19:18:04 -0300

    Cumprindo o requisito 18

--- script.js
+++ script.js
@@ -2,6 +2,10 @@ const login = document.getElementById('login');
 const password = document.getElementById('password');
 const loginButton = document.getElementById('login-button');
 
+const checkAgreement = document.getElementById('agreement');
+
+document.getElementById('submit-btn').disabled = true;
+
 function submitLogin() {
   if (login.value === 'tryber@teste.com' && password.value === '123456') {
     alert('Olá, Tryber!');
@@ -10,4 +14,11 @@ function submitLogin() {
   }
 }
 
+function enableBtn() {
+  if (checkAgreement.disabled === false) {
+    document.getElementById('submit-btn').disabled = false;
+  }
+}
+
 loginButton.addEventListener('click', submitLogin);
+checkAgreement.addEventListener('change', enableBtn);
