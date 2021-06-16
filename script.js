4296aefa42eb082e16689a818ebcd8eb55c1bbbc
Gabriel-Lobin <gabrielsuassuna@hotmail.com>
2021-06-16 16:37:29 -0300

    Exercises 1 ,2 ,3 e 4

---
+++ script.js
@@ -0,0 +1,13 @@
+
+function validacaoEmailSenha() {
+  const login = document.getElementById('login').value;
+  const senha = document.getElementById('senha').value;
+  const loginPadrao = 'tryber@teste.com';
+  const senhaPadrao = '123456';
+  if (login !== loginPadrao || senha !== senhaPadrao) {
+    alert('Login ou senha inválidos.');
+  } else {
+    alert('Olá, Tryber!');
+  }
+}
+document.querySelector('.login').addEventListener('click', validacaoEmailSenha);
