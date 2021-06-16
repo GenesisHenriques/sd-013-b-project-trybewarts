a57b5bb4ab60d50900d14e9db4dffa1452a16cd9
eaeniko <eaenikolai@gmail.com>
2021-06-16 16:22:35 -0300

    [Nikolai] lint

--- script.js
+++ script.js
@@ -3,7 +3,7 @@ const trybePassword = document.getElementById('trybe-password');
 const submitBtn = document.getElementById('submit-btn');
 
 function validateLogin() {
-  if (trybeEmail.value != 'tryber@teste.com' || trybePassword.value != '123456') {
+  if (trybeEmail.value !== 'tryber@teste.com' || trybePassword.value !== '123456') {
     alert('Login ou senha inválidos');
   } else {
     alert('Olá, Tryber!');
