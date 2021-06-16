db415f23d95c99d048a3d7a709b54a6b302f61bd
Carol_Inglethe <carolinainglethe@gmail.com>
2021-06-16 16:51:26 -0300

    Requesito 3- Formulario de autenticaçao na barra superior

---
+++ script.js
@@ -0,0 +1,14 @@
+// Requesito 3 - autenticando formulario e seus inputs
+const inputLogin = document.getElementById('input-login');
+const inputSenha = document.getElementById('input-senha');
+const botaoEntrar = document.getElementById('button-entrar');
+
+function login() {
+    if (inputLogin.value !== 'tryber@teste.com' && inputSenha.value !== '123456') {
+        alert('Login ou senha inválidos.')
+    }
+    if (inputLogin.value === 'tryber@teste.com' && inputSenha.value === '123456') {
+        alert('Olá, Tryber!')
+    }
+}
+botaoEntrar.addEventListener('click', login);
