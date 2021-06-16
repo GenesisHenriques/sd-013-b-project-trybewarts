5132478150fb8cef6317eea53f6ab5dced3c87d8
eaeniko <eaenikolai@gmail.com>
2021-06-16 16:38:00 -0300

    [Nikolai] Header CSS

--- script.js
+++ script.js
@@ -3,7 +3,6 @@ const trybePassword = document.getElementById("trybe-password");
 const submitBtn = document.getElementById("submit-btn");
 
 function validateLogin() {
-
   if (
     trybeEmail.value !== "tryber@teste.com" ||
     trybePassword.value !== "123456"
