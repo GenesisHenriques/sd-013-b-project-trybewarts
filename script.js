5640b650c9fd603fab1c0ed4202aa5b8525309fe
Joao Victor <joaogmavictor@gmail.com>
2021-06-16 16:56:43 -0300

    Corregindo Lint

--- script.js
+++ script.js
@@ -1,26 +1,31 @@
 //  Realiza autenticação de login
 function verifyLogin() {
-  let login = document.getElementsByName('login')[0].value;
-  let password = document.getElementsByName('password')[0].value;
+  const login = document.getElementsByName('login')[0].value;
+  const password = document.getElementsByName('password')[0].value;
 
-  let validCredentials = {login: 'tryber@teste.com',
-  password: '123456'};
+  const validCredentials = {
+    login: 'tryber@teste.com', password: '123456',
+  };
 
   let loginVerified = false;
   let passwordVerified = false;
 
   //  Valida o login
-  if(login === validCredentials.login) {
+  if (login === validCredentials.login) {
     loginVerified = true;
   }
-  if(password === validCredentials.password) {
+  if (password === validCredentials.password) {
     passwordVerified = true;
   }
 
   //  Mostra a validação ao usuário
-  if (loginVerified && passwordVerified){
+  if (loginVerified && passwordVerified) {
     alert('Olá, Tryber!');
   } else {
     alert('Login ou senha inválidos.');
   }
 }
+
+const btnLogar = document.querySelector('#btn-login');
+
+btnLogar.addEventListener('click', verifyLogin);
