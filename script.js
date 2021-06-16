3dcfd30f43a8d1089ab23e123b0d93ac2aaf5d87
Alexsander-Trybe <Alexsander81002745@gmail.com>
2021-06-16 16:39:42 -0300

    Requisisto 3 em progresso - Alexs

---
+++ script.js
@@ -0,0 +1,14 @@
+function checkLoginAndPassword(event) {
+  const login = document.getElementById('input-login').value;
+  const password = document.getElementById('input-password').value;
+  console.log(login);
+  console.log(password);
+
+  if (login === 'tryber@teste.com' && password === '123456') {
+    alert('Olá, Tryber!');
+  } else {
+    alert('Login ou senha inválidos.');
+  }
+}
+
+document.getElementById('button-check').addEventListener('click', checkLoginAndPassword);
