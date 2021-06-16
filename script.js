c46c31b3901df5d866082c4c517b65a032d4a87d
Arthur-Jr <arthurjr1906@gmail.com>
2021-06-16 16:57:16 -0300

    Adiciona html do décimo oitavo requisito.

--- script.js
+++ script.js
@@ -1,5 +1,7 @@
 const inputLogin = document.querySelector('#login');
 const inputPassword = document.querySelector('#password');
+const agreement = document.querySelector('#agreement');
+const button = document.querySelector('#submit-btn');
 
 document.querySelector('#btn-login').addEventListener('click', () => {
   if (inputLogin.value === 'tryber@teste.com' && inputPassword.value === '123456') {
@@ -7,4 +9,15 @@ document.querySelector('#btn-login').addEventListener('click', () => {
   } else {
     alert('Login ou senha inválidos.');
   }
+});
+
+agreement.addEventListener('change', () => {
+  if (button.disabled === true) {
+    button.disabled = false;
+  } else {
+    button.disabled = true;
+  }
+ 
+  // Aprendi sobre button.disabled, neste link
+  // https://flexiple.com/disable-button-javascript/
 });
