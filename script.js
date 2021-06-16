a5a7ba6eedb704dd9f88293192c0da75e0528f4f
Lucas Caribé <lucasmoc47@gmail.com>
2021-06-16 18:50:00 -0300

    Habilitando e desabilitando botao de submit + Adicionando contador de caracteres + Pequenas alteraçoes

--- script.js
+++ script.js
@@ -1,12 +1,33 @@
+const maxTextLength = 500;
+
 // validação de login
-const logarButton = document.getElementById('logar');
-function validateLogin() {
-  const login = document.getElementsByTagName('input')[0].value;
-  const senha = document.getElementsByTagName('input')[1].value;
+const loginButton = document.getElementById('logar');
+
+loginButton.addEventListener('click', () => {
+  const login = document.querySelector('#login-input').value;
+  const senha = document.querySelector('#password-input').value;
+
   if (login !== 'tryber@teste.com' || senha !== '123456') {
     alert('Login ou senha inválidos.');
   } else {
     alert('Olá, Tryber!');
   }
-}
-logarButton.addEventListener('click', validateLogin);
+});
+
+// habilitando e desabilitando botão de submit
+const agreementCheckbox = document.querySelector('#agreement');
+
+agreementCheckbox.addEventListener('change', (event) => {
+  const submitButton = document.querySelector('#submit-btn');
+
+  submitButton.disabled = !event.target.checked;
+});
+
+// contador de caracteres
+const textarea = document.querySelector('textarea');
+
+textarea.addEventListener('keyup', (event) => {
+  const counter = document.querySelector('#counter');
+
+  counter.innerText = maxTextLength - event.target.value.length;
+});
