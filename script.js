6241f32e849f29de054e98537fb923e90e65c3e1
Matheus Figueiredo <mathhsoares2@live.com>
2021-06-16 18:25:08 -0300

    Requisito 18

--- script.js
+++ script.js
@@ -1,6 +1,8 @@
 const login = document.getElementById('input-login');
 const password = document.getElementById('input-password');
 const btnLogin = document.getElementById('btn-login');
+const checkForm = document.getElementById('agreement');
+const btnForm = document.getElementById('submit-btn');
 
 function checkCredentials() {
   // prettier-ignore
@@ -8,4 +10,10 @@ function checkCredentials() {
   else alert('Login ou senha inválidos.');
 }
 
+function showBtn() {
+  btnForm.disabled = checkForm.checked !== true;
+}
+
 btnLogin.addEventListener('click', checkCredentials);
+
+checkForm.addEventListener('change', showBtn);
