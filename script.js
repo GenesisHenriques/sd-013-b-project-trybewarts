f693d76e4fb0f5b1521c268b00216ef9891a84b7
Lucas Caribé <lucasmoc47@gmail.com>
2021-06-16 19:24:19 -0300

    Validando nome, sobrenome e email

--- script.js
+++ script.js
@@ -3,7 +3,9 @@ const maxTextLength = 500;
 // validação de login
 const loginButton = document.getElementById('logar');
 
-loginButton.addEventListener('click', () => {
+loginButton.addEventListener('click', (event) => {
+  event.preventDefault();
+
   const login = document.querySelector('#login-input').value;
   const senha = document.querySelector('#password-input').value;
 
@@ -30,4 +32,46 @@ textarea.addEventListener('keyup', (event) => {
   const counter = document.querySelector('#counter');
 
   counter.innerText = maxTextLength - event.target.value.length;
+});
+
+// salvando informações do formulário
+function validateName(inputId) {
+  const nameInput = document.querySelector(inputId);
+  const name = nameInput.value.trim();
+
+  if (!name.length) {
+    throw new Error('Erro! Nome inválido');
+  }
+
+  return name;
+}
+
+function validateEmail() {
+  const emailInput = document.querySelector('#input-email');
+  const email = emailInput.value.trim();
+  const emailPattern = /^[a-zA-Z0-9._-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/;
+
+  if (!email.match(emailPattern)) {
+    throw new Error('Erro! Email inválido');
+  }
+
+  return email;
+}
+
+const submitButton = document.querySelector('#submit-btn');
+
+submitButton.addEventListener('click', (event) => {
+  event.preventDefault();
+
+  try {
+    const formData = {
+      name: validateName('#input-name'),
+      lastName: validateName('#input-lastname'),
+      email: validateEmail(),
+    };
+
+    console.log(formData);
+  } catch (error) {
+    alert(error.message);
+  }
 });
