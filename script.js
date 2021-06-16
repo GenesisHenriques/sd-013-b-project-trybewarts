bf9bfa7eae07304020d3366b5d45d4d26ed97152
RafaelReisTM <srp.rafael@gmail.com>
2021-06-16 17:57:07 -0300

    requisito 3, 4, 5 e 6 completos

--- script.js
+++ script.js
@@ -2,12 +2,13 @@ const login = document.querySelector('#login');
 const password = document.querySelector('#password');
 const bttn = document.querySelector('#bttn');
 
-bttn.addEventListener('click', validEmail);
-function validEmail (event) {
-  if (login.value === "tryber@teste.com" || password.value === "123456") {
+function validEmail() {
+  if (login.value === 'tryber@teste.com' && password.value === '123456') {
     alert('Olá, Tryber!');
   }
-  else {
-    alert('Login ou senha inválidos');
+  if (login.value !== 'tryber@teste.com' || password.value !== '123456') {
+    alert('Login ou senha inválidos.');
   }
 }
+
+bttn.addEventListener('click', validEmail);
