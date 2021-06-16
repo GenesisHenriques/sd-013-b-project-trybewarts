30d0cb3877cfe29befa18e38f3ef3e7031867d31
Leonardo <leo_mc_inter@hotmail.com>
2021-06-16 16:26:26 -0300

    Corrigindo formatação

---
+++ script.js
@@ -0,0 +1,12 @@
+// validação de login
+const logarButton = document.getElementById("logar");
+function validateLogin() {
+  const login = document.getElementsByTagName("input")[0].value;
+  const senha = document.getElementsByTagName("input")[1].value;
+  if (login !== "tryber@teste.com" || senha !== "123456") {
+    alert("Login ou senha inválidos.");
+  } else {
+    alert("Olá, Tryber!");
+  }
+}
+logarButton.addEventListener("click", validateLogin);
