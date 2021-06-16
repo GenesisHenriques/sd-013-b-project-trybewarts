59fdf3d18a564da63a3bd6b220dc71912b7cb109
João Victor Rodrigues Del Papa <joaodelpapa1@gmail.com>
2021-06-16 16:38:45 -0300

    Requisito 7

---
+++ script.js
@@ -0,0 +1,16 @@
+const buttonLogin = document.getElementById('login-btn');
+
+function checkLogin(event) {
+  const email = document.getElementById('input-email');
+  const senha = document.getElementById('input-senha');
+
+  event.preventDefault();
+
+  if (email.value !== 'tryber@teste.com' || senha.value !== '123456') {
+    alert('Login ou senha inválidos.');
+  } else {
+    alert('Olá, Tryber!');
+  }
+}
+
+buttonLogin.addEventListener('click', checkLogin);
