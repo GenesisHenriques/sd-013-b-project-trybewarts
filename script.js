f87e856371025a96a084154b7ce1f166a56656cb
PedroSpelta <p.h.s.8989@hotmail.com>
2021-06-16 19:11:47 -0300

    fazendo o priemiro req bonus

--- script.js
+++ script.js
@@ -17,6 +17,15 @@ loginButton.addEventListener('click', handleLogin);
 
 const agreement = document.querySelector('#agreement');
 const submitButton = document.querySelector('#submit-btn');
+submitButton.addEventListener('click', (event) => {
+  const form = document.querySelector('#evaluation-form');
+  const name = document.querySelector('#input-name');
+  const lastname = document.querySelector('#input-lastname');
+  console.log(`${name.value} ${lastname.value}`);
+  form.Nome = `${name.value} ${lastname.value}`;
+  event.preventDefault();
+});
+
 agreement.addEventListener('click', (event) => {
   const { checked } = event.target;
   if (checked === true) {
@@ -26,3 +35,12 @@ agreement.addEventListener('click', (event) => {
   }
 });
 //  submitButton.addEventListener('click', handleSubmit);
+
+const counter = document.querySelector('#counter');
+const textArea = document.querySelector('#textarea');
+textArea.addEventListener('keyup', (event) => {
+  const { target } = event;
+  const textCount = 500 - Number(target.value.length);
+  counter.innerText = textCount;
+  console.log(textCount);
+});
