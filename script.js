7a894ebff758bcaa7f5df131d196f1a44fab7576
RafaelReisTM <srp.rafael@gmail.com>
2021-06-16 18:48:14 -0300

    requisito 7, 8, 9, 10 e 11 corrigidos

--- script.js
+++ script.js
@@ -4,18 +4,11 @@ const bttn = document.querySelector('#bttn');
 
 function validEmail() {
   if (login.value === 'tryber@teste.com' && password.value === '123456') {
-<<<<<<< HEAD
-     alert('Olá, Tryber!');
-  } if ((login.value !== 'tryber@teste.com' || password.value !== '123456')) {
-    alert('Login ou senha inválidos.');
-}
-=======
     alert('Olá, Tryber!');
   }
   if (login.value !== 'tryber@teste.com' || password.value !== '123456') {
     alert('Login ou senha inválidos.');
   }
->>>>>>> bf9bfa7eae07304020d3366b5d45d4d26ed97152
 }
 
 bttn.addEventListener('click', validEmail);
