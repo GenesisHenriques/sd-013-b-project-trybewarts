6c397f73e0aff30ecd0c1b298964188cd786b4ac
Antônio Gonçalves <tonygoldmarket@yahoo.com>
2021-06-16 16:37:52 -0300

    [Antonio] Arrumando os erros do linter

--- script.js
+++ script.js
@@ -3,9 +3,8 @@ const trybePassword = document.getElementById('trybe-password');
 const submitBtn = document.getElementById('submit-btn');
 
 function validateLogin() {
-
   if (
-    trybeEmail.value !== 'tryber@teste.com' 
+    trybeEmail.value !== 'tryber@teste.com'
     || trybePassword.value !== '123456') {
     alert('Login ou senha inválidos');
   } else {
