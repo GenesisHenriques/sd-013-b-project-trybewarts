c0aba98e27b9b9d0e4b2d6ab4499d3c70bbf0f0c
Thales Vinicius <thalessilva.tv@gmail.com>
2021-06-16 19:36:45 -0300

    debugs

--- script.js
+++ script.js
@@ -1,6 +1,8 @@
 const loginButton = document.getElementById('login-button');
 const loginUsername = document.querySelector('#login-username');
 const loginPassword = document.querySelector('#login-password');
+const submitButton = document.querySelector('#submit-btn');
+const infoCheckbox = document.querySelector('#agreement');
 
 function loginVerification() {
   if (loginUsername.value !== 'tryber@teste.com') {
@@ -13,3 +15,14 @@ function loginVerification() {
 }
 
 loginButton.addEventListener('click', loginVerification);
+
+function ableButton() {
+  if (infoCheckbox.checked !== false) {
+    submitButton.disabled = false;
+  }
+  else {
+    submitButton.disabled = true;
+  }
+}
+
+infoCheckbox.addEventListener('click', ableButton);
