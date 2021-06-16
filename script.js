aa29180a0faa580381ede5358beb0c385062162b
José Demeneghi <josedemeneghi_cba@hotmail.com>
2021-06-16 16:04:00 -0400

    exercicio 3

--- script.js
+++ script.js
@@ -1,13 +1,10 @@
-const butao = document.getElementsByClassName('forms');
-const login =document.getElementById('login');
-const senha =document.getElementById('senha');
+const login = document.getElementById('login');
+const senha = document.getElementById('senha');
 
-function alerta (){
+function entrar() {
   if (login.value === 'tryber@teste.com' && senha.value === '123456') {
     alert('Olá, Tryber!');
   } else {
     alert('Login ou senha inválidos.');
   }
 }
-
-butao.addeventlistener('click', alerta);
