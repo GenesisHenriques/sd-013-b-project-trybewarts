9e7550bab6a6f0ee478c8af9fb1ddd90ef25ba84
Jeferson Oliveira <jeferson.contas@gmail.com>
2021-06-16 18:53:03 -0300

    Requisitos 5-6, 8-16

--- script.js
+++ script.js
@@ -1,18 +1,32 @@
 const email = document.querySelector('#input-login-email');
 const password = document.querySelector('#input-login-password');
 const loginButton = document.querySelector('#loginButton');
+const rateContainer = document.querySelector('#rate-container');
 
 function login() {
   const validEmail = email.value;
   const validPassword = password.value;
-  const emailFormat = 'tryber@teste.com';
-  const passwordFormat = '123456';
-  if ((validEmail !== emailFormat) || (validPassword !== passwordFormat)) {
-    email.value = '';
-    password.value = '';
-    alert('Login ou senha inválidos.');
-  } else {
+  if (validEmail === 'tryber@teste.com' && validPassword === '123456') {
     alert('Olá, Tryber!');
+  } else {
+    alert('Login ou senha inválidos.');
   }
 }
+
 loginButton.addEventListener('click', login);
+
+function createRateRadios() {
+  const rates = 10;
+  for (let index = 1; index <= rates; index += 1) {
+    const radioRate = document.createElement('input');
+    const labelRate = document.createElement('label');
+    radioRate.type = 'radio';
+    radioRate.name = 'rate';
+    radioRate.value = index;
+    labelRate.innerText = index;
+    rateContainer.appendChild(radioRate);
+    rateContainer.appendChild(labelRate);
+  }
+}
+
+createRateRadios();
