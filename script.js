7fcfcc1c9475cc8e880a018b4d1a451da5680e71
Barros99 <jeanmeneses@hotmail.com>
2021-06-16 20:00:31 -0300

    Desafios 18 e 19 finalizados

--- script.js
+++ script.js
@@ -1,8 +1,20 @@
-document.querySelector('.login').addEventListener('click', () => {
+/* document.querySelector('.login').addEventListener('click', () => {
   const inputLogin = document.getElementById('login').value;
   const inputSenha = document.getElementById('senha').value;
 
   return inputLogin !== 'tryber@teste.com' || inputSenha !== '123456'
     ? alert('Login ou senha inválidos.')
     : alert('Olá, Tryber!');
+}); */
+
+document.getElementById('agreement').addEventListener('click', () => {
+  const check = document.getElementById('agreement');
+  const button = document.getElementById('submit-btn');
+  if (check.value === 'false') {
+    button.removeAttribute('disabled');
+    check.value = 'true';
+  } else {
+    button.disabled = true;
+    check.value = 'false';
+  }
 });
