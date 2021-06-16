7e11d669ed1eafc7ee3eab9130ea4a02fbaa852d
Antônio Gonçalves <tonygoldmarket@yahoo.com>
2021-06-16 16:00:14 -0300

    [Antonio] Validacao de e-mail e senha concluidos(R3)

---
+++ script.js
@@ -0,0 +1,13 @@
+const trybeEmail = document.getElementById('trybe-email');
+const trybePassword = document.getElementById('trybe-password');
+const submitBtn = document.getElementById('submit-btn');
+
+function validateLogin() {
+  if (trybeEmail.value != 'tryber@teste.com' || trybePassword.value != '123456') {
+    alert('Login ou senha inválidos');
+  } else {
+    alert('Olá, Tryber!');
+  }
+}
+
+submitBtn.addEventListener('click', validateLogin)
