8c99f8e3ebdfa1de90dcd65b908ac56eda5961dc
alanmdf <alanmoreirascr@gmail.com>
2021-06-16 19:08:14 -0300

    Requisitos 7 ao 19 finalizados,evaluator e linter

--- script.js
+++ script.js
@@ -27,22 +27,35 @@ function validatingLogin() {
 // Fim de Trecho de Código do Genesis
 
 // Inicio de Trecho de código do Alan
-generateRateButtons();
-
-function generateRateButtons () {
+function generateRateButtons() {
   const rateDiv = document.getElementsByClassName('rate-div');
   for (let index = 1; index < 11; index += 1) {
     const radioBtn = document.createElement('input');
     const radioBtnLabel = document.createElement('label');
-    radioBtn.setAttribute('type','radio');
-    radioBtn.setAttribute('name','rate');
+    radioBtn.setAttribute('type', 'radio');
+    radioBtn.setAttribute('name', 'rate');
     radioBtn.setAttribute('value', index);
-    radioBtn.setAttribute('id', index)
-    radioBtnLabel.setAttribute('for', index)
+    radioBtn.setAttribute('id', index);
+    radioBtnLabel.setAttribute('for', index);
     radioBtnLabel.innerText = index;
     rateDiv[0].appendChild(radioBtn);
     rateDiv[0].appendChild(radioBtnLabel);
   }
 }
 
+generateRateButtons();
+
+const agreementCheckBox = document.getElementById('agreement');
+const submitBtn = document.getElementById('submit-btn');
+
+function checkAgreement() {
+  if (agreementCheckBox.checked === true) {
+    submitBtn.disabled = false;
+  } else {
+    submitBtn.disabled = true;
+  }
+}
+
+checkAgreement();
+agreementCheckBox.addEventListener('click', checkAgreement);
 // Fim de Trecho de Código do Alan
