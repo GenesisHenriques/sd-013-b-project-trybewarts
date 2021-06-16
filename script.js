6b8539870698ed70a9d9351010b4774696d0a84b
Rodrigo Marchi Silva <rodrigomarchisilva@gmail.com>
2021-06-16 18:27:36 -0300

    Início do Requisito 21

--- script.js
+++ script.js
@@ -20,3 +20,40 @@ agreement.addEventListener('change', () => {
   // Aprendi sobre button.disabled, neste link
   // https://flexiple.com/disable-button-javascript/
 });
+
+button.onclick = function () {
+
+  const subjectArray = document.querySelectorAll('input[type="checkbox"]:checked');
+
+  let subjectList = '';
+  for (let index = 0; index < subjectArray.length; index += 1) {
+    if (subjectList === '') {
+      subjectList = `${subjectArray[index].value}`;
+    } else {
+      subjectList = subjectList + `, ${subjectArray[index].value}`;
+    }
+  }
+  
+  const dataArray = [
+  'Nome: ', `${document.getElementById('input-name').value} `, `${document.getElementById('input-lastname').value}`,
+  'Email: ', `${document.getElementById('input-email').value}`, '',
+  'Casa: ', `${document.getElementById('house').value}`, '',
+  'Família: ', `${document.querySelector('input[name="family"]:checked').value}`, '',
+  'Matérias: ', `${subjectList}`, '',
+  'Avaliação: ', `${document.querySelector('input[name="rate"]:checked').value}`, '',
+  'Observações: ', `${document.getElementById('textarea-box').value}`, ''
+  ]
+
+  const enteredDataArea = document.getElementById('main-content');
+  enteredDataArea.innerHTML = '';
+  const newUl = document.createElement('ul');
+  newUl.className = 'entered-data-area'
+  enteredDataArea.appendChild(newUl);
+
+  for (let index = 0; index < dataArray.length; index += 3) {
+    const ul = document.querySelector('.entered-data-area');
+    let newLi = document.createElement('li');
+    newLi.innerText = `${dataArray[index]}${dataArray[index + 1]}${dataArray[index + 2]}`;
+    ul.appendChild(newLi);
+  }
+}
