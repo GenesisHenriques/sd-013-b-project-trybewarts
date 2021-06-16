ff8e79dd8dac3ed1bdfb0ef3eb2ddba00425a8b0
Rodrigo Augusto <rodrigoaugusto96@outlook.com>
2021-06-16 15:52:12 -0300

    Requisito 3

---
+++ script.js
@@ -0,0 +1,11 @@
+let botaoEnviar = document.getElementById('botao');
+
+botaoEnviar.addEventListener('click', () => {
+  let login = document.getElementById('login').value;
+  let senha = document.getElementById('senha').value;
+  if (login === 'tryber@teste.com' && senha === '123456') {
+    alert('Olá, Tryber!');
+  } else {
+    alert('Login ou senha inválidos.');
+  }
+});
