0300f2723e39dbb2fb8dc85a75e3d6327811fba8
PedroSpelta <p.h.s.8989@hotmail.com>
2021-06-16 14:57:53 -0300

    criando primeiros arquivos

---
+++ script.js
@@ -0,0 +1,8 @@
+function handleLogin() {
+  const userInput = document.querySelector('#username');
+  const passwordInput = document.querySelector('#password');
+  if (userInput.value === 'tryber@teste.com' && passwordInput.value === '123456') {
+    return window.alert('Olá, Tryber!');
+  }
+  return window.alert('Login ou senha inválidos.');
+}
