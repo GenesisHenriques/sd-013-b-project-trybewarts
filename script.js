bff181c49a60f033db4126f22e3b87b68a482b88
noel-srocha <manoelrocha.neto.93@hotmail.com>
2021-06-17 11:44:32 -0300

    Challenge 1-19 completed

--- script.js
+++ script.js
@@ -1,13 +1,24 @@
-const botao = document.querySelector('.btn');
+const loginBtn = document.querySelector('.btn');
+const submitBtn = document.querySelector('#submit-btn');
+const agreementCheckBox = document.querySelector('#agreement');
 
-function Login() {
-  const usuario = document.querySelector('.username').value;
-  const senha = document.querySelector('.password').value;
-  if (usuario === 'tryber@teste.com' && senha === '123456') {
+function performLogin() {
+  const user = document.querySelector('.username').value;
+  const password = document.querySelector('.password').value;
+  if (user === 'tryber@teste.com' && password === '123456') {
     window.alert('Olá, Tryber!');
   } else {
     window.alert('Login ou senha inválidos.');
   }
 }
 
-botao.addEventListener('click', Login);
+function checkUserAgreement() {
+  if (!agreementCheckBox.checked) {
+    submitBtn.disabled = true;
+  } else {
+    submitBtn.disabled = false;
+  }
+}
+
+loginBtn.addEventListener('click', performLogin);
+agreementCheckBox.addEventListener('change', checkUserAgreement);
