3082d575d78c24cf6b5447023dde1b7772772908
Gustavo Alves Miyazaki <gustavo.alves388@gmail.com>
2021-06-17 10:38:50 -0300

    Desafios 1 ao 19 completo

--- script.js
+++ script.js
@@ -1,6 +1,8 @@
 const email = document.getElementById('input-login');
 const password = document.getElementById('input-senha');
 const loginButton = document.getElementById('entrar');
+const agreementCheckBox = document.getElementById('agreement');
+const sendButton = document.getElementById('submit-btn');
 
 function checkEmail() {
   for (let letter = 0; letter < email.value.length; letter += 1) {
@@ -27,3 +29,13 @@ function checkLogin() {
 }
 
 loginButton.addEventListener('click', checkLogin);
+
+function enableSendButton() {
+  if (agreementCheckBox.checked == true){
+  sendButton.disabled = false;
+  } else {
+  sendButton.disabled = true;
+  }
+}
+
+agreementCheckBox.addEventListener('click', enableSendButton);
