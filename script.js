40e0b1452712d14b4645f4aba56a41f10ef69937
Guilherme Giacomini Teixeira <ggiacomini2012@gmail.com>
2021-06-17 12:53:58 -0300

    conclui o requisito 20

--- script.js
+++ script.js
@@ -29,3 +29,19 @@ function enable() {
 }
 
 enable();
+
+// Cria um contador para o textera
+// Pesquisando:
+// Neste consegui ajuda para o css:
+// source: http://jsfiddle.net/oyhf3812/
+// source: https://www.w3schools.com/jsref/event_onkeyup.asp
+// source: https://stackoverflow.com/questions/14086398/count-textarea-characters
+// A resposta estava aqui:
+// source: https://stackoverflow.com/questions/24875414/addeventlistener-change-and-option-selection
+const counter = document.querySelector('#counter');
+const textarea = document.querySelector('#textarea');
+
+// O addEventListener input aciona toda vez que o elemento recebe um input
+textarea.addEventListener('input', () => {
+  counter.innerText = `${500 - textarea.value.length}/500`;
+});
