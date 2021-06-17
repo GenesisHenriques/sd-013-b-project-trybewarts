e5cb9ce0efb2af2d20946c30db196b0650a3f38c
Amós Rodrigues <amos.adm.rh@gmail.com>
2021-06-16 22:17:33 -0300

    realiza requisito 18

--- script.js
+++ script.js
@@ -1,18 +1,25 @@
-const button = document.querySelector('#button');
-const nome = document.querySelector('#nome');
-const password = document.querySelector('#password');
+// const button = document.querySelector('#button');
+// const nome = document.querySelector('#nome');
+// const password = document.querySelector('#password');
+// function validationLogin() {
+//   const inputName = document.querySelector('#nome').value;
+//   const inputPassword = document.querySelector('#password').value;
+//   if (inputName !== 'tryber@teste.com' || inputPassword !== '123456') {
+//     alert('Login ou senha inválidos.');
+//   } else {
+//     alert('Olá, Tryber!');
+//   }
+// }
 
-function validationLogin() {
-  const inputName = nome.value;
-  const inputPassword = password.value;
+document.querySelector('#button').addEventListener('click', () => {
+  const inputName = document.querySelector('#nome').value;
+  const inputPassword = document.querySelector('#password').value;
   if (inputName !== 'tryber@teste.com' || inputPassword !== '123456') {
     alert('Login ou senha inválidos.');
   } else {
     alert('Olá, Tryber!');
   }
-}
-
-button.addEventListener('click', validationLogin);
+});
 
 function enableBtnSubmit(checkAccept) {
   const submitBtn = document.getElementById('submit-btn');
