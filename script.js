f952d4b8636da917274d8bf474e8db73f94c7622
Letícia Castro <aicitelks@gmail.com>
2021-06-17 13:55:26 -0300

    Desenvolvendo o requisito 20

--- script.js
+++ script.js
@@ -1,10 +1,17 @@
-// window.onload = function () {
-// };
+window.onload = function () {
+  divComentario.append('Caracteres restantes: ');
+};
 
 const botaoEnviar = document.getElementById('submit-btn');
 botaoEnviar.disabled = true;
-
 const checboxAgreement = document.querySelector('#agreement');
+let divComentario = document.querySelector('#comentario');
+
+/* let textarea = document.querySelector('#textarea');
+let textoCompleto = document.querySelector('#textarea').value;
+let qtdMaxCaracteres = 500;
+let contadorResultado = document.createElement('label');
+let contador = 5; */
 
 function verificarCheckboxAgreement() {
   // se check = true (ou seja, se marcado)
@@ -17,3 +24,28 @@ function verificarCheckboxAgreement() {
   }
 }
 checboxAgreement.addEventListener('click', verificarCheckboxAgreement);
+
+/* 
+function contarCaracteresTextarea() {
+  contador = contador - 1;
+  divComentario.append(contador + ' ');
+  console.log(textoCompleto);
+}
+
+textarea.addEventListener('input', contarCaracteresTextarea); */
+
+let contador = 5;
+let desc = document.querySelector("#textarea");
+desc.addEventListener("keypress", function(e) {
+  let maxChars = 5;
+  
+  contador = contador - 1;
+  inputLength = desc.value.length;
+  
+  if(inputLength >= maxChars) {
+    e.preventDefault();
+  }
+  
+  divComentario.append(contador + ' ');
+
+});
