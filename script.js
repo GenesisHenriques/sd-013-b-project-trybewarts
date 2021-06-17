757a0039b7fec2aa704ec96f3e28c19aa34d82a9
RafaelReisTM <srp.rafael@gmail.com>
2021-06-17 13:42:48 -0300

    Requisito 18

--- script.js
+++ script.js
@@ -1,6 +1,8 @@
 const login = document.querySelector('#login');
 const password = document.querySelector('#password');
 const bttn = document.querySelector('#bttn');
+const sendBttn = document.querySelector('#submit-btn');
+const checkBox = document.querySelector('#agreement');
 
 function validEmail() {
   if (login.value === 'tryber@teste.com' && password.value === '123456') {
@@ -12,3 +14,13 @@ function validEmail() {
 }
 
 bttn.addEventListener('click', validEmail);
+
+function termsChanged() {
+  if (this.checked) {
+    sendBttn.disabled = false;
+  } else {
+    sendBttn.disabled = true;
+  }
+}
+
+checkBox.addEventListener('click', termsChanged);
