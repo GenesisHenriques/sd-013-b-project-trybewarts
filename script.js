046e5842bf0ab7e1b462ceee341622c014ac1fe0
Lucas T Franca <dev.lucasteixeira@gmail.com>
2021-06-17 13:02:20 -0300

    Requisito 21 Finalizado

--- script.js
+++ script.js
@@ -29,12 +29,57 @@ function createCount(event) {
   labelCount.innerText = 500 - eventValue.length;
 }
 
+function checkedFamily() {
+  const family = document.getElementsByName('family');
+  for (let key = 0; key < family.length; key += 1) {
+    if (family[key].checked) {
+      return family[key].value;
+    }
+  }
+}
+
+function checkedContent() {
+  const content = document.getElementsByName('content');
+  const allContent = [];
+  for (let key = 0; key < content.length; key += 1) {
+    if (content[key].checked) {
+      allContent.push(` ${content[key].value}`);
+    }
+  }
+  return String(allContent);
+}
+
+function checkedRate() {
+  const rate = document.getElementsByName('rate');
+  for (let key = 0; key < rate.length; key += 1) {
+    if (rate[key].checked) {
+      return rate[key].value;
+    }
+  }
+}
+
+function submitForm(event) {
+  event.preventDefault();
+  const family = checkedFamily();
+  const content = checkedContent();
+  const rate = checkedRate();
+  const form = document.getElementById('evaluation-form');
+  const name = document.getElementById('input-name');
+  const lastName = document.getElementById('input-lastname');
+  const email = document.getElementById('input-email');
+  const house = document.getElementById('house');
+  const textarea = document.getElementById('textarea');
+  form.innerHTML = `Nome: ${name.value} ${lastName.value} <br> Email: ${email.value} <br> Casa: ${house.value} <br> Família: ${family} <br> Matérias: ${content} <br> Avaliação: ${rate} <br> Observações: ${textarea.value}`;
+}
+
 window.onload = () => {
   const buttonLogin = document.getElementById('buttonLogin');
+  const buttonSubmit = document.getElementById('submit-btn');
   const checkboxAgreement = document.getElementById('agreement');
   const textarea = document.getElementById('textarea');
 
   buttonLogin.addEventListener('click', checkLogin);
+  buttonSubmit.addEventListener('click', submitForm);
   checkboxAgreement.addEventListener('change', verifyAgreement);
   textarea.addEventListener('keyup', createCount);
 };
