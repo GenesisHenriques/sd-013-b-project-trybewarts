f3d508b2c4c4fdf50e064263dbbd7621aca93683
Rodrigo <rodrigo.fidencio@gmail.com>
2021-06-16 21:14:55 -0300

    Requisitos 1 ao 20 funcionando, lint formatado

--- script.js
+++ script.js
@@ -1,9 +1,8 @@
 const login = document.getElementById('login');
 const password = document.getElementById('password');
 const loginButton = document.getElementById('login-button');
-
+const count = document.querySelector('textarea');
 const checkAgreement = document.getElementById('agreement');
-
 const submitBTN = document.getElementById('submit-btn');
 
 submitBTN.disabled = true;
@@ -26,6 +25,21 @@ function enableBtn() {
   }
 }
 
+function counter() {
+  const text = document.querySelector('textarea').value;
+  const counter1 = document.querySelector('#counter');
+  let contador = 0;
+  let resultado = 0;
+  counter1.innerHTML = '500';
+  for (let index = 0; index < text.length; index += 1) {
+    contador += 1;
+    const max = 500;
+    resultado = max - contador;
+    counter1.innerHTML = resultado;
+  }
+}
+
 loginButton.addEventListener('click', submitLogin);
 checkAgreement.addEventListener('click', enableBtn);
+count.addEventListener('input', counter);
 window.onload = checkAgreement;
