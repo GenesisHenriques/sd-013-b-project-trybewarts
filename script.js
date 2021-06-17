ed05ee1070de2f5d5032e4efe95d87c955e2c599
Vivian <ost.vivian@gmail.com>
2021-06-16 23:20:13 -0300

    Ajustes de lint

--- script.js
+++ script.js
@@ -1,10 +1,10 @@
-const login = document.getElementById('login-btn')
+const login = document.getElementById('login-btn');
 
 function getLogin() {
-  const email = document.getElementById('email').value; 
+  const email = document.getElementById('email').value;
   const password = document.getElementById('password').value;
-  if (email === 'tryber@teste.com' && password === '123456'){
-    alert('Olá, Tryber!')
+  if (email === 'tryber@teste.com' && password === '123456') {
+    alert('Olá, Tryber!');
   } else {
     alert('Login ou senha inválidos.');
   }
