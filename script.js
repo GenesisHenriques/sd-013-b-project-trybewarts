d3b785823b1ac7720c02642a0b2b8d093c296200
Letícia Castro <aicitelks@gmail.com>
2021-06-17 18:08:52 -0300

    Ajustes Lint no script

--- script.js
+++ script.js
@@ -1,8 +1,16 @@
 window.onload = function () {
-  divComentario.append('Caracteres restantes: ');
+  
 };
 
+/** VARIÁVEIS */
+
+const botaoEnviar = document.getElementById('submit-btn');
+botaoEnviar.disabled = true;
+const checboxAgreement = document.querySelector('#agreement');
+let divComentario = document.querySelector('#comentario');
 const login = document.getElementById('login-btn');
+
+/** FUNÇÕES */
 
 function getLogin() {
   const email = document.getElementById('email').value;
@@ -15,12 +23,6 @@ function getLogin() {
 }
 
 login.addEventListener('click', getLogin);
-
-
-const botaoEnviar = document.getElementById('submit-btn');
-botaoEnviar.disabled = true;
-const checboxAgreement = document.querySelector('#agreement');
-let divComentario = document.querySelector('#comentario');
 
 /* let textarea = document.querySelector('#textarea');
 let textoCompleto = document.querySelector('#textarea').value;
@@ -39,6 +41,7 @@ function verificarCheckboxAgreement() {
   }
 }
 checboxAgreement.addEventListener('click', verificarCheckboxAgreement);
+divComentario.append('Caracteres restantes: ');
 
 /* 
 function contarCaracteresTextarea() {
@@ -47,20 +50,21 @@ function contarCaracteresTextarea() {
   console.log(textoCompleto);
 }
 
-textarea.addEventListener('input', contarCaracteresTextarea); */
+textarea.addEventListener('input', contarCaracteresTextarea); 
+*/
 
 let contador = 5;
-let desc = document.querySelector("#textarea");
-desc.addEventListener("keypress", function(e) {
-  let maxChars = 5;
-  
+const desc = document.querySelector('#textarea');
+desc.addEventListener('keypress', function(e) {
+  const maxChars = 5;
+  let inputLength;
+
   contador = contador - 1;
   inputLength = desc.value.length;
   
-  if(inputLength >= maxChars) {
+  if (inputLength >= maxChars) {
     e.preventDefault();
   }
   
-  divComentario.append(contador + ' ');
-
+  divComentario.append(contador);
 });
