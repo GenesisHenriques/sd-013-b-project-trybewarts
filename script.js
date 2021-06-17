ac86ab78dc6a0185b560a556d64fd4d7e1329718
GuCanalle <gucanalle@gmail.com>
2021-06-16 23:14:32 -0300

    Concluido requisito 20

--- script.js
+++ script.js
@@ -3,6 +3,9 @@ const loginButton = document.querySelector('#login-button'); // seleciona botão
 const passwordInput = document.querySelector('#password'); // seleciona input da senha
 const agreeCheckbox = document.querySelector('#agreement'); // seleciona o checkbox de concordo com os termos
 const agreeBtn = document.querySelector('#submit-btn'); // seleciona o botão de enviar no final do formulário
+const textArea = document.querySelector('#textarea'); // seleciona a text area
+const counter = document.querySelector('#counter'); // seleciona o counter de caracteres
+
 function verifyLogin() {
   if (loginInput.value !== 'tryber@teste.com') return false;
   if (passwordInput.value !== '123456') return false;
@@ -21,3 +24,8 @@ agreeCheckbox.addEventListener('change', () => {
   agreeBtn.classList.toggle('btn-disabled');
   agreeBtn.classList.toggle('btn-enabled');
 });
+
+textArea.addEventListener('input', (e) => {
+  let cont = 500 - textArea.value.length;
+  counter.innerHTML = cont;
+})
