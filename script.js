0e3f0e2d9f3092738931fb2c2e50afbb10451d8f
Matheus Figueiredo <mathhsoares2@live.com>
2021-06-17 11:05:30 -0300

    Requisito Bonus 20

--- script.js
+++ script.js
@@ -3,6 +3,8 @@ const password = document.getElementById('input-password');
 const btnLogin = document.getElementById('btn-login');
 const checkForm = document.getElementById('agreement');
 const btnForm = document.getElementById('submit-btn');
+const text = document.getElementById('textarea');
+const counter = document.getElementById('counter');
 
 function checkCredentials() {
   // prettier-ignore
@@ -14,6 +16,13 @@ function showBtn() {
   btnForm.disabled = checkForm.checked !== true;
 }
 
+function textCount() {
+  const count = 500 - text.value.length;
+  counter.textContent = count;
+}
+
 btnLogin.addEventListener('click', checkCredentials);
 
 checkForm.addEventListener('change', showBtn);
+
+text.addEventListener('keyup', textCount);
