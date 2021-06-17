9db6804be0165c4b3a13c84f7fb23b4b38aef71e
Amós Rodrigues <amos.adm.rh@gmail.com>
2021-06-16 22:34:14 -0300

    realiza requsitos 18 e 19

--- script.js
+++ script.js
@@ -1,16 +1,3 @@
-// const button = document.querySelector('#button');
-// const nome = document.querySelector('#nome');
-// const password = document.querySelector('#password');
-// function validationLogin() {
-//   const inputName = document.querySelector('#nome').value;
-//   const inputPassword = document.querySelector('#password').value;
-//   if (inputName !== 'tryber@teste.com' || inputPassword !== '123456') {
-//     alert('Login ou senha inválidos.');
-//   } else {
-//     alert('Olá, Tryber!');
-//   }
-// }
-
 document.querySelector('#button').addEventListener('click', () => {
   const inputName = document.querySelector('#nome').value;
   const inputPassword = document.querySelector('#password').value;
@@ -21,7 +8,8 @@ document.querySelector('#button').addEventListener('click', () => {
   }
 });
 
-function enableBtnSubmit(checkAccept) {
+function enableBtnSubmit() {
+  const checkAccept = document.getElementById('agreement');
   const submitBtn = document.getElementById('submit-btn');
   if (checkAccept.checked === true) {
     submitBtn.disabled = false;
