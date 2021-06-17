3cc890c3771252513650fbac38f10493b21b8bd1
Isaddora Bayma <isadora.bayma@gmail.com>
2021-06-17 18:17:37 -0300

    requisito 20 lint

--- script.js
+++ script.js
@@ -10,7 +10,6 @@ function login() {
 }
 const bntEntrar = document.querySelector('#btn-entrar');
 bntEntrar.addEventListener('click', login);
-
 // CRIA SELECT PARA CASA //
 function criaCasas(nome, id) {
   const select = document.querySelector('#house');
@@ -23,29 +22,23 @@ criaCasas('Gitnória', 'gitnoria-house');
 criaCasas('Reactpuff', 'reactpuff-house');
 criaCasas('Corvinode', 'corvinode-house');
 criaCasas('Pytherina', 'pytherina-house');
-
 // ATIVA BOTÃO DE ENVIAR //
 // const condição = document.querySelector('#agreement');
 // const button = document.querySelector('#submit-btn');
-
 // function ativaBtn(event) {
 //   if (condição.checked) {
 //     event.preventDefault(button);
 // }
 // };
-
 // function addEvento() {
 //   condição.addEventListener('checked', ativaBtn);
 // }
 // addEvento();
-
 // CONTADOR
 const myTextArea = document.getElementById('text-area');
 const charsTest = document.getElementById('counter');
 const charsMax = 500;
-
 myTextArea.addEventListener('input', () => {
   const restante = charsMax - myTextArea.value.length;
   charsTest.textContent = `${restante} caracteres restantes`;
 });
-
