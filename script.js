c72929ca6f2d92c07d98b42e0ed563b77f514c6f
Lucas T Franca <dev.lucasteixeira@gmail.com>
2021-06-16 23:55:37 -0300

    Requisito 17, 18 Finalizados

--- script.js
+++ script.js
@@ -12,8 +12,20 @@ function checkLogin(event) {
   console.log(typeof inputLogin, typeof inputPassword);
 }
 
+function verifyAgreement() {
+  const buttonSubmit = document.getElementById('submit-btn');
+
+  if (buttonSubmit.disabled) {
+    buttonSubmit.disabled = false;
+  } else {
+    buttonSubmit.disabled = true;
+  }
+}
+
 window.onload = () => {
   const buttonLogin = document.getElementById('buttonLogin');
+  const checkboxAgreement = document.getElementById('agreement');
 
   buttonLogin.addEventListener('click', checkLogin);
+  checkboxAgreement.addEventListener('change', verifyAgreement);
 };
