6e29b272f3661b97a764544bb3d67e0240b9ebc4
Lucas A. Cardim <lucascardim777@gmail.com>
2021-06-17 14:26:44 -0300

    Requisito 18 resolvido e projeto finalizado!

--- script.js
+++ script.js
@@ -1,6 +1,9 @@
 const userEmail = document.querySelector('#user-email');
 const userPass = document.querySelector('#input-pass');
 const buttonLogin = document.querySelector('#input-login');
+const submit = document.querySelector('#submit-btn');
+const checkButton = document.querySelector('#agreement');
+
 function validLogin() {
   if (userEmail.value !== 'tryber@teste.com') {
     return false;
@@ -19,3 +22,22 @@ buttonLogin.addEventListener('click', (event) => {
     alert('Login ou senha inválidos.');
   }
 });
+
+buttonLogin.addEventListener('click', (event) => {
+  event.preventDefault();
+  if (validLogin()) {
+    alert('Olá, Tryber!');
+  } else {
+    alert('Login ou senha inválidos.');
+  }
+});
+function checkedin() {
+  const check = checkButton.checked;
+  const btn = submit;
+  if (check === true) {
+    btn.disabled = false;
+  } else {
+    btn.disabled = true;
+  }
+}
+checkButton.addEventListener('click', checkedin);
