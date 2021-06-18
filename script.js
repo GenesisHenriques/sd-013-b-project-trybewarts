3a8aae9f2d544dcfba7c678e16da5c8917cfc796
Polyana Balotin <polybalotin@hotmail.com>
2021-06-18 14:15:27 -0300

    login

--- script.js
+++ script.js
@@ -1,10 +1,23 @@
+const inputLogin = document.querySelector('#login');
+const inputPassword = document.querySelector('#password');
+const loginButton = document.querySelector('#buttonLogin');
 const submitBtn = document.querySelector('#submit-btn');
 const agreementBox = document.querySelector('#agreement');
-function checkUserAgreement() {
-  if (!agreementBox.checked) {
-    submitBtn.disabled = true;
+
+function authenticateLogin() {
+  if (inputLogin.value === 'tryber@teste.com' && inputPassword.value === '123456') {
+    alert('Olá, Tryber!');
   } else {
-    submitBtn.disabled = false;
+    alert('Login ou senha inválidos.');
   }
 }
-agreementBox.addEventListener('change', checkUserAgreement);
+loginButton.addEventListener('click', authenticateLogin);
+
+function checkUserAgreement() {
+  if (agreementBox.checked === true) {
+    submitBtn.disabled = false;
+  } else {
+    submitBtn.disabled = true;
+  }
+}
+submitBtn.addEventListener('click', checkUserAgreement);
