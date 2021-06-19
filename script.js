02c04b0c112b0c4cb16a51276481963b7c522351
Barros99 <jeanmeneses@hotmail.com>
2021-06-19 11:28:08 -0300

    Ajustando css e requisito 20

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
-document.querySelector('.login').addEventListener('click', () => {
+document.querySelector('.login').addEventListener('click', (e) => {
+  e.preventDefault();
   const inputLogin = document.getElementById('login').value;
   const inputSenha = document.getElementById('senha').value;
-
   return inputLogin !== 'tryber@teste.com' || inputSenha !== '123456'
     ? alert('Login ou senha inválidos.')
     : alert('Olá, Tryber!');
@@ -17,4 +17,14 @@ document.getElementById('agreement').addEventListener('click', () => {
     button.disabled = true;
     check.value = 'false';
   }
+});
+
+document.getElementById('textarea').addEventListener('keyup', (event) => {
+  const counter = document.getElementById('counter');
+  counter.innerText = 500 - event.target.value.length;
+});
+
+document.getElementById('submit-btn').addEventListener('click', () => {
+  const name = document.getElementById('input-name');
+  name.innerText = name.value;
 });
