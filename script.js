2596f874efc7c344a71f498a7016f20c888e3eef
Dororops <leeurw@hotmail.com>
2021-06-20 03:58:45 -0300

    Adicionando Desafio-20

--- script.js
+++ script.js
@@ -21,12 +21,10 @@ checkbox.onclick = function canSendForm() {
   sendButton.toggleAttribute('disabled');
 };
 
-
 // Textarea words count
-
 const textArea = document.getElementById('textarea');
 
-textArea.onkeyup = function countLetters () {
-  let counter = document.getElementById('counter');
+textArea.onkeyup = function countLetters() {
+  const counter = document.getElementById('counter');
   counter.innerText = 500 - textArea.value.length;
-}
+};
