a0420b5f1ec4c9c1f2dbb1e7b738e3624bb1007a
Barros99 <jeanmeneses@hotmail.com>
2021-06-19 22:20:43 -0300

    Questao 21 resolvida

--- script.js
+++ script.js
@@ -1,3 +1,12 @@
+const nome = document.getElementById('input-name');
+const lastName = document.getElementById('input-lastname');
+const email = document.getElementById('input-email');
+const house = document.getElementById('house');
+const family = document.querySelectorAll('.radio-family');
+const content = document.querySelectorAll('.subject');
+const rate = document.querySelectorAll('.radio-rate');
+const textarea = document.getElementById('textarea');
+
 document.querySelector('.login').addEventListener('click', (e) => {
   e.preventDefault();
   const inputLogin = document.getElementById('login').value;
@@ -24,7 +33,48 @@ document.getElementById('textarea').addEventListener('keyup', (event) => {
   counter.innerText = 500 - event.target.value.length;
 });
 
-document.getElementById('submit-btn').addEventListener('click', () => {
-  const name = document.getElementById('input-name');
-  name.innerText = name.value;
+function getFullName(nameElement, lastNameElement, textTitle) {
+  const pAnswer = document.createElement('p');
+  pAnswer.innerText = `${textTitle}: ${nameElement.value} ${lastNameElement.value}`;
+  return pAnswer;
+}
+
+function getELement(element, textTitle) {
+  const pAnswer = document.createElement('p');
+  pAnswer.innerText = `${textTitle}: ${element.value}`;
+  return pAnswer;
+}
+
+function getElementsRadio(elements, textTitle) {
+  for (let index = 0; index < elements.length; index += 1) {
+    if (elements[index].type === 'radio' && elements[index].checked) {
+      return getELement(elements[index], textTitle);
+    }
+  }
+}
+
+function getElementsCheckbox(elements, textTitle) {
+  const valorP = [];
+  const pAnswer = document.createElement('p');
+  for (let index = 0; index < elements.length; index += 1) {
+    if (elements[index].type === 'checkbox' && elements[index].checked) {
+      valorP.push(` ${elements[index].value}`);
+    }
+  }pAnswer.innerText = `${textTitle}: ${valorP}`;
+  return pAnswer;
+}
+
+document.getElementById('submit-btn').addEventListener('click', (e) => {
+  e.preventDefault();
+  const divAnswer = document.createElement('div');
+  const form = document.getElementById('evaluation-form');
+  divAnswer.appendChild(getFullName(nome, lastName, 'Nome'));
+  divAnswer.appendChild(getELement(email, 'Email'));
+  divAnswer.appendChild(getELement(house, 'Casa'));
+  divAnswer.appendChild(getElementsRadio(family, 'Família'));
+  divAnswer.appendChild(getElementsCheckbox(content, 'Matérias'));
+  divAnswer.appendChild(getElementsRadio(rate, 'Avaliação'));
+  divAnswer.appendChild(getELement(textarea, 'Observações'));
+  form.innerHTML = '';
+  form.appendChild(divAnswer);
 });
