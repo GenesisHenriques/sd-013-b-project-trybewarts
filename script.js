2e6efa3a0486152e63adf403ed4f02bf79c6a4db
diogodlemos <diogodrummond@hotmail.com>
2021-06-21 19:31:40 -0300

    Requisito 21

--- script.js
+++ script.js
@@ -25,3 +25,53 @@ function countChar() {
   document.getElementById('counter').innerText = available;
 }
 textArea.addEventListener('keyup', countChar);
+
+const button = document.getElementById('submit-btn');
+function getFullName() {
+    const name = document.getElementById('input-name');
+  const lastName = document.getElementById('input-lastname');
+}
+function getInformation(event) {
+  event.preventDefault();
+  const formMain = document.getElementById('evaluation-form');
+  const name = document.getElementById('input-name');
+  const lastName = document.getElementById('input-lastname');
+  const email = document.getElementById('input-email');
+  const casa = document.getElementById('house');
+  const familia = document.getElementsByClassName('family');
+  let checkedValue = null;
+  for (const key in familia) {
+    if (familia[key].checked) {
+      checkedValue = familia[key].value;
+    }
+  }
+  const subject = document.getElementsByClassName('subject');
+  let subjectChecked = '';
+  // subject.addEventListener('click',selectSubjects);
+  function selectSubjects() {
+    for (const key in subject) {
+      if (subject[key].checked) {
+        subjectChecked += `${subject[key].value}, `;
+      }
+    }
+  }
+  selectSubjects();
+  const rate = document.getElementsByClassName('rate');
+  let rateChecked = null;
+  for (const key in rate) {
+    if (rate[key].checked) {
+      rateChecked = rate[key].value;
+    }
+  }
+  formMain.innerHTML = '';
+  const information = document.createElement('p');
+  information.innerText = `Nome: ${name.value} ${lastName.value}
+  Email: ${email.value}
+  Casa: ${casa.value}
+  Família: ${checkedValue}
+  Matérias: ${subjectChecked}
+  Avaliação: ${rateChecked}
+  Observações: ${textArea.value}`;
+  formMain.appendChild(information);
+}
+button.addEventListener('click', getInformation);
