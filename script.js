4b829629e8bf00476a52ec9bbf7bfcdf4b2b0116
Ismael_Luviseti <ismaelluviseti@gmail.com>
2021-06-21 10:35:31 -0300

    Requisito 20 finalizado

--- script.js
+++ script.js
@@ -3,6 +3,7 @@ const password = document.getElementById('input-senha');
 const loginButton = document.getElementById('entrar');
 const agreementCheckBox = document.getElementById('agreement');
 const sendButton = document.getElementById('submit-btn');
+let textarea = document.getElementById('textarea');
 
 function checkEmail() {
   for (let letter = 0; letter < email.value.length; letter += 1) {
@@ -38,4 +39,11 @@ function enableSendButton() {
   }
 }
 
+function counter(texto) {
+  document.getElementById('counter').innerHTML = 500 - texto.target.value.length;
+}
+
+textarea.addEventListener('keyup', counter);
 agreementCheckBox.addEventListener('click', enableSendButton);
+
+
