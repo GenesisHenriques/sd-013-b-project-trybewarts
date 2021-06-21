9a5113bb4a093ab756b1886881793f5e5487ba49
AC-Oliveira <allancardoso21@gmail.com>
2021-06-21 12:40:51 -0300

    Resolvido ate o requisito 19

--- script.js
+++ script.js
@@ -1,7 +1,8 @@
 const loginButton = document.getElementById('login-button');
 const login = document.getElementById('login');
 const password = document.getElementById('password');
-// const familyParent = document.getElementById('label-family');
+const agreementCheck = document.getElementById('agreement');
+const submitBtn = document.getElementById('submit-btn');
 
 loginButton.addEventListener('click', () => {
   if (login.value !== 'tryber@teste.com' && password.value !== 123456) {
@@ -11,15 +12,10 @@ loginButton.addEventListener('click', () => {
   }
 });
 
-// const sun2 = document.createElement('label');
-
-// sun2.innerHTML = '<input type="radio" name="family" id="front-end" value="Frontend">Família Frontend';
-// familyParent.appendChild(sun2);
-
-// const sun3 = document.createElement('label');
-// sun3.innerHTML = '<input type="radio" name="family" id="back-end" value = "Backend">Família Backend';
-// familyParent.appendChild(sun3);
-
-// const sun4 = document.createElement('label');
-// sun4.innerHTML = '<input type="radio" name="family" id="full-stack" value="FullStack">Família FullStack';
-// familyParent.appendChild(sun4);
+agreementCheck.addEventListener('click', () => {
+  if (agreementCheck.checked === true) {
+    submitBtn.disabled = false;
+  } else {
+    submitBtn.disabled = true;
+  }
+});
