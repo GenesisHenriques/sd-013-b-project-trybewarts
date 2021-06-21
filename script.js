fa54f175a11ed4f968295cca07583bdd93904cbe
Letícia Castro <aicitelks@gmail.com>
2021-06-21 14:31:54 -0300

    Requisito 21 semi-funcional

--- script.js
+++ script.js
@@ -11,6 +11,7 @@ const maxCaracteres = 500;
 let contagem = 0;
 
 const textarea = document.querySelector('#textarea');
+const respostas = document.querySelector('#respostas');
 
 /** FUNÇÕES */
 
@@ -23,7 +24,6 @@ function getLogin() {
     alert('Login ou senha inválidos.');
   }
 }
-
 login.addEventListener('click', getLogin);
 
 function verificarCheckboxAgreement() {
@@ -44,3 +44,90 @@ function contarCaracteres() {
   campoContagem.innerText = contagem;
 }
 textarea.addEventListener('keyup', contarCaracteres);
+
+function salvarRespostas() {
+  const nome = document.querySelector('#input-name').value;
+  const sobrenome = document.querySelector('#input-lastname').value;
+  const email = document.querySelector('#input-email').value;
+  const casa = document.querySelector('#house').value;
+  let radiosFamilia = document.getElementsByName('family');
+  let familiaEscolhida = '';
+  let conteudo = document.querySelectorAll('.subject');
+  let conteudoEscolhido = [];
+  let radiosAvaliacao = document.getElementsByName('rate');
+  let notaAvaliacao = '';
+  comentario = textarea.value;
+
+  for (let i = 0; i < radiosFamilia.length; i += 1) {
+    if (radiosFamilia[i].checked) {
+      familiaEscolhida = radiosFamilia[i].value;
+    }
+  }
+
+  for (let i = 0; i < conteudo.length; i += 1) {
+    if (conteudo[i].checked) {
+      conteudoEscolhido.push(conteudo[i].value);
+    }
+  }
+
+  for (let i = 0; i < radiosAvaliacao.length; i += 1) {
+    if (radiosAvaliacao[i].checked) {
+      notaAvaliacao = radiosAvaliacao[i].value;
+    }
+  }
+
+  localStorage.setItem('nome', nome);
+  localStorage.setItem('sobrenome', sobrenome);
+  localStorage.setItem('email', email);
+  localStorage.setItem('casa', casa);
+  localStorage.setItem('familia', familiaEscolhida);
+  localStorage.setItem('conteudo', conteudoEscolhido);
+  localStorage.setItem('avaliacao', notaAvaliacao);
+  localStorage.setItem('observacao', comentario);
+
+}
+botaoEnviar.addEventListener('click', salvarRespostas);
+
+window.onload = function () {
+
+  if (localStorage.length != 0) {
+    let formulario1 = document.querySelector('#evaluation-form');
+    formulario1.remove();
+
+    let main = document.querySelector('.main');
+    let novoFormulario = document.createElement('form');
+
+    novoFormulario.id = 'evaluation-form';
+    main.append(novoFormulario);
+
+    let item1 = document.createElement('p');
+    let item2 = document.createElement('p');
+    let item4 = document.createElement('p');
+    let item5 = document.createElement('p');
+    let item6 = document.createElement('p');
+    let item7 = document.createElement('p');
+    let item8 = document.createElement('p');
+
+    item1.innerText = `Nome: ${localStorage.getItem('nome')} ${localStorage.getItem('sobrenome')}`;
+    novoFormulario.append(item1);
+
+    item2.innerText = `Email: ${localStorage.getItem('email')}`;
+    novoFormulario.append(item2);
+
+    item4.innerText = `Casa: ${localStorage.getItem('casa')}`;
+    novoFormulario.append(item4);
+
+    item5.innerText = `Família: ${localStorage.getItem('familia')}`;
+    novoFormulario.append(item5);
+
+    item6.innerText = `Matérias: ${localStorage.getItem('conteudo')}`;
+    novoFormulario.append(item6);
+
+    item7.innerText = `Avaliação: ${localStorage.getItem('avaliacao')}`;
+    novoFormulario.append(item7);
+
+    item8.innerText = `Observações: ${localStorage.getItem('observacao')}`;
+    novoFormulario.append(item8);
+
+  }
+}
