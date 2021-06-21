ec8582e37e50fbc2ce1cd00702b92ded7b99905a
José Demeneghi <josedemeneghi_cba@hotmail.com>
2021-06-21 14:37:30 -0400

    ultimo commit correção

--- script.js
+++ script.js
@@ -10,13 +10,3 @@ function entrar() {
 }
 
 console.log(entrar);
-
-
-// Com o template literals
-console.log(`Primeira linha;
-Segunda linha;
-Terceira linha;`
-)
-
-// Sem o template literals:
-console.log('Primeira linha;\n' + 'Segunda linha;\n' + 'Terceira linha;\n')
