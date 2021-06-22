b21d543a98b9cf20d6ac96df057dd909cba399ad
Aleilton Chavenco <aleilton@gmail.com>
2021-06-21 23:59:49 -0300

    Requisito 18

--- script.js
+++ script.js
@@ -3,19 +3,28 @@ const defaultPassword = '123456';
 const btnLogar = document.getElementById('btn-logar');
 const inputLogin = document.getElementById('input-login');
 const inputPassword = document.getElementById('input-password');
+const agreement = document.getElementById('agreement');
+const btnSubmit = document.getElementById('submit-btn');
 
-function validateLogin (login, password) {
+const validateLogin = (login, password) => {
   if (login === defaultLogin && password === defaultPassword) {
     alert(`Olá, Tryber!`);
   } else {
     alert(`Login ou senha inválidos.`);
   }
-}
+};
 
-function login () {
-  validateLogin(inputLogin.value, inputPassword.value);
-}
+const login = ()  => validateLogin(inputLogin.value, inputPassword.value);
+
+const iAgree = () => {
+  if (agreement.checked === true) {
+    btnSubmit.removeAttribute('disabled');
+  } else {
+    btnSubmit.setAttribute('disabled', 'disabled');
+  }
+};
 
 window.onload = function() {
   btnLogar.addEventListener('click', login);
+  agreement.addEventListener('click', iAgree);
 };
