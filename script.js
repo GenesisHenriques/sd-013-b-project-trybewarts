6f4236177384b5af2a9e56c81add093a93b1ea58
Letícia Castro <aicitelks@gmail.com>
2021-06-21 21:28:35 -0300

    Ajustes do Lint

--- script.js
+++ script.js
@@ -25,11 +25,11 @@ let novoFormulario = document.createElement('form');
 
 let item1 = document.createElement('p');
 let item2 = document.createElement('p');
+let item3 = document.createElement('p');
 let item4 = document.createElement('p');
 let item5 = document.createElement('p');
 let item6 = document.createElement('p');
 let item7 = document.createElement('p');
-let item8 = document.createElement('p');
 
 /** FUNÇÕES */
 
@@ -63,59 +63,58 @@ function contarCaracteres() {
 }
 textarea.addEventListener('keyup', contarCaracteres);
 
-function salvarRespostas(event) {
-  /* cancela o evento, sem parar a sua execução, ou seja, faz com a página não seja recarregada, porem implementa o resto da função */
-  event.preventDefault();
+function salvarRespostas(event) {  
+  event.preventDefault();/* cancela o evento, sem parar a sua execução, ou seja, faz com a página não seja recarregada, porem implementa o resto da função */
+  for (let i = 0; i < radiosFamilia.length; i += 1) {
+    if (radiosFamilia[i].checked) {
+      familiaEscolhida = radiosFamilia[i].value;
+    }
+  }
+  for (let i = 0; i < conteudo.length; i += 1) {
+    if (conteudo[i].checked) {
+      conteudoEscolhido.push(' ' + conteudo[i].value);
+    }
+  }
+  for (let i = 0; i < radiosAvaliacao.length; i += 1) {
+    if (radiosAvaliacao[i].checked) {
+      notaAvaliacao = radiosAvaliacao[i].value;
+    }
+  }  
+  imprimirFormularioPreenchido();
+  criarNovoFormulario();
+}
+botaoEnviar.addEventListener('click', salvarRespostas);
+
+function criarNovoFormulario() {
+  formulario1.remove();
+  novoFormulario.id = 'evaluation-form';
+  main.append(novoFormulario);
+}
+
+function imprimirFormularioPreenchido() {
   let nome = document.querySelector('#input-name').value;
   let sobrenome = document.querySelector('#input-lastname').value;
   let email = document.querySelector('#input-email').value;
   let casa = document.querySelector('#house').value;
   comentario = textarea.value;
 
-  for (let i = 0; i < radiosFamilia.length; i += 1) {
-    if (radiosFamilia[i].checked) {
-      familiaEscolhida = radiosFamilia[i].value;
-    }
-  }
-
-  for (let i = 0; i < conteudo.length; i += 1) {
-    if (conteudo[i].checked) {
-      conteudoEscolhido.push(' ' + conteudo[i].value);
-    }
-  }
-
-  for (let i = 0; i < radiosAvaliacao.length; i += 1) {
-    if (radiosAvaliacao[i].checked) {
-      notaAvaliacao = radiosAvaliacao[i].value;
-    }
-  }
+  item1.innerText = `Nome: ${nome} ${sobrenome}`;  
+  item2.innerText = `Email: ${email}`;  
+  item3.innerText = `Casa: ${casa}`;  
+  item4.innerText = `Família: ${familiaEscolhida}`;
+  item5.innerText = `Matérias: ${conteudoEscolhido}`;
+  item6.innerText = `Avaliação: ${notaAvaliacao}`;
+  item7.innerText = `Observações: ${comentario}`;
   
-  formulario1.remove();
-  novoFormulario.id = 'evaluation-form';
-  main.append(novoFormulario);
+  return  novoFormulario.appendChild(item1),
+          novoFormulario.appendChild(item2),
+          novoFormulario.appendChild(item3),
+          novoFormulario.appendChild(item4),
+          novoFormulario.appendChild(item5),
+          novoFormulario.appendChild(item6),
+          novoFormulario.appendChild(item7);
 
-  item1.innerText = `Nome: ${nome} ${sobrenome}`;
-  novoFormulario.appendChild(item1);
-
-  item2.innerText = `Email: ${email}`;
-  novoFormulario.appendChild(item2);
-
-  item4.innerText = `Casa: ${casa}`;
-  novoFormulario.appendChild(item4);
-
-  item5.innerText = `Família: ${familiaEscolhida}`;
-  novoFormulario.appendChild(item5);
-
-  item6.innerText = `Matérias: ${conteudoEscolhido}`;
-  novoFormulario.appendChild(item6);
-
-  item7.innerText = `Avaliação: ${notaAvaliacao}`;
-  novoFormulario.appendChild(item7);
-
-  item8.innerText = `Observações: ${comentario}`;
-  novoFormulario.appendChild(item8);
 }
-botaoEnviar.addEventListener('click', salvarRespostas);
 
 /* Opção salvando tudo no localStorage presente no gitHub, porem não passava no requisito, pois os teste exige que as informações sejam adicionadas
 no momento do clique, ou seja, junto com o evento */
