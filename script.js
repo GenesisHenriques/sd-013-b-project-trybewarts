64519d321996eddd5bf3a54615d71579850a31b4
Pedro Brito <pedroab0119@gmail.com>
2021-06-24 23:03:26 -0300

    Req21 done - finaly done?

--- script.js
+++ script.js
@@ -7,7 +7,6 @@ submitBtn.disabled = true;
 const agreementChk = document.querySelector('#agreement');
 const textarea = document.querySelector('#textarea');
 const counter = document.querySelector('#counter');
-console.log(counter.innerText);
 
 function verifyLogin() {
   const loginCorrect = 'tryber@teste.com';
@@ -29,7 +28,11 @@ function generateRadiosBtn() {
     rateLabel.innerText = index;
     rate.type = 'radio';
     rate.name = 'rate';
+    rate.className = 'rate';
     rate.value = index;
+    if (index === 2) {
+      rate.checked = true;
+    }
 
     yourRate.appendChild(rate);
     yourRate.appendChild(rateLabel);
@@ -44,10 +47,71 @@ function isAgreementCheck(event) {
   }
 }
 
+function setLocalStorage() {
+  // localStorage.setItem('datas', JSON.stringify(arrayItens));
+  console.log('a');
+  localStorage.setItem('datas', 'oi');
+}
+
 btnLogin.addEventListener('click', verifyLogin);
 generateRadiosBtn();
 agreementChk.addEventListener('click', isAgreementCheck);
 textarea.addEventListener('keyup', (event) => {
   counter.innerText = (500 - event.target.textLength);
-  console.log(event.target.textLength);
+});
+
+function groupDatas() {
+  const inputName = document.querySelector('#input-name');
+  const inputLastname = document.querySelector('#input-lastname');
+  const fullNameText = `Nome: ${inputName.value} ${inputLastname.value}`;
+  
+  const inputEmail = document.querySelector('#input-email');
+  const inputEmailText = `Email: ${inputEmail.value}`;
+
+  const house = document.querySelector('#house');
+  const houseText = `Casa: ${house.value}`;
+
+  const family = document.querySelectorAll('.family');
+  let familyText = '';
+  for (let key of family) {
+    if (key.checked) {
+      familyText = `Família: ${key.value}`;
+    }
+  }
+
+  const techs = document.querySelectorAll('.subject');
+  let techsText = 'Matérias: ';
+  for (let key of techs) {
+    if (key.checked) {
+      techsText += `${key.value}, `;
+    }
+  }
+  techsText = techsText.slice(0, -2);
+  techsText += '.';
+
+  const labelRate = document.querySelectorAll('.rate');
+  let labelRateText = '';
+  for (let key of labelRate) {
+    if (key.checked) {
+      labelRateText = `Avaliação: ${key.value}`;
+    }
+  }
+
+  // const textarea = document.querySelector('#textarea');
+  const textareaText = `Observações: ${textarea.value}`;
+
+  const evaluationForm = document.querySelector('#evaluation-form');
+  evaluationForm.innerHTML = `
+  ${fullNameText}<br>
+  ${inputEmailText}<br>
+  ${houseText}<br>
+  ${familyText}<br>
+  ${techsText}<br>
+  ${labelRateText}<br>
+  ${textareaText}<br>
+  `;
+}
+
+submitBtn.addEventListener('click', (event) => {
+  groupDatas();
 });
