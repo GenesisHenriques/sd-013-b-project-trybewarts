bd2862b8b48a72ccd1de8fe8c35f8169db8694ba
Rafael Nery Machado <rafalenerymachado@gmail.com>
2021-07-16 17:37:12 -0300

    requisito 21 - concluido

--- script.js
+++ script.js
@@ -13,6 +13,54 @@ function submitLogin(event) {
   alert('Olá, Tryber!');
 }
 
+function checkboxValue(checked, value, oldValue) {
+  if (checked) {
+    return oldValue ? [...oldValue, value] : [value];
+  }
+  return oldValue;
+}
+
+function extractValue({ checked, type, value }, oldValue) {
+  if (type === 'checkbox') {
+    return checkboxValue(checked, value, oldValue);
+  }
+  if (type === 'radio') {
+    return checked ? value : oldValue;
+  }
+  return value;
+}
+
+function mountFormData(form) {
+  return [...form].reduce((formData, dado) => ({
+    ...formData,
+    [dado.name]: extractValue(dado, formData[dado.name]),
+  }), {});
+}
+
+// function mountFormData(form) {
+//   return [...form].reduce((formData, dado) => {
+//     console.log(formData);
+//     return {
+//       ...formData,
+//       [dado.name]: extractValue(dado, formData[dado.name]),
+//     };
+//   }, {});
+// }
+
+function submitEvaluation(event) {
+  event.preventDefault();
+  const form = event.target;
+  const formData = mountFormData(form);
+
+  form.innerHTML = `Nome: ${formData.name} ${formData.lastname}
+    Email: ${formData.email}
+    Casa: ${formData.casa}
+    Família: ${formData.family}
+    Matérias: ${formData.content ? formData.content.join(', ') : ''}
+    Avaliação: ${formData.rate}
+    Observações: ${formData.comment}`;
+}
+
 function toggleButton() {
   const submitButton = document.getElementById('submit-btn');
   submitButton.disabled = !submitButton.disabled;
@@ -30,6 +78,10 @@ window.onload = () => {
   document
     .querySelector('.trybewarts-login')
     .addEventListener('submit', submitLogin);
+
+  document
+    .querySelector('#evaluation-form')
+    .addEventListener('submit', submitEvaluation);
 
   document.getElementById('agreement').addEventListener('click', toggleButton);
 
